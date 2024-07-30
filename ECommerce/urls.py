50a1afd14e4ce86df386034a9956512ee0c3f7f6
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-07-30 20:26:18 +0330

    changed

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('newuser/', include('home.urls'))
-
+    path('newuser/', include('home.urls')),
+    path('admin/', admin.site.urls),
 ]
